06ffce32a2539e5954b0fb9b5bdc8a4ab9d36352
Felix Kressmann <felix@felixkr.de>
2021-12-11 23:21:42 +0100

    Enhance admin interface (creation / deletion) and slot overview

--- src/models/Timeslot.py
+++ src/models/Timeslot.py
@@ -5,7 +5,6 @@ from extensions.db import db
 class Timeslot(db.Model):
     id = db.Column(db.String(36), primary_key=True, default=generate_uuid)
     start_time = db.Column(db.DateTime)
-    end_time = db.Column(db.DateTime)
     capacity = db.Column(db.Integer)
 
     def __str__(self):
--- src/routes/web.py
+++ src/routes/web.py
@@ -7,13 +7,14 @@ from models.Timeslot import Timeslot
 from models.Booking import Booking
 from requests.Reservation import Reservation
 from services.mail_service import send_mail
+from util import *
 
 web_bp = Blueprint('web', __name__)
 
 
 @web_bp.route('/')
 def index():
-    timeslots: [Timeslot] = Timeslot.query.all()
+    timeslots: [Timeslot] = Timeslot.query.order_by(Timeslot.start_time).all()
     return render_template('index.html', timeslots=timeslots)
 
 
@@ -44,12 +45,40 @@ def confirm():
     if not maybe_booking:
         return "Reservation not found", 404
 
-    maybe_booking.ack_at = datetime.datetime.now()
+    if not maybe_booking.ack_at:
+        maybe_booking.ack_at = datetime.datetime.now()
     db.session.commit()
     return render_template("confirmation.html", reservation=maybe_booking)
 
 
 @web_bp.route('/admin-6a3522f3-6c6e-4b0e-a044-d653177fbb91')
 def admin():
-    timeslots = Timeslot.query.all()
+    timeslots = Timeslot.query.order_by(Timeslot.start_time).all()
     return render_template("admin.html", timeslots=timeslots)
+
+
+@web_bp.route('/admin-create-6a3522f3-6c6e-4b0e-a044-d653177fbb91', methods=['POST'])
+def admin_create():
+    r = Reservation(request.form)
+    if r.valid:
+        booking = Booking(name=r.name,
+                          email=r.email,
+                          timeslot=r.timeslot,
+                          ack_at=datetime.datetime.now())
+        db.session.add(booking)
+        db.session.commit()
+
+        return redirect(url_for('web.admin'))
+    else:
+        return "Invalid request!", 400
+
+
+@web_bp.route('/delete')
+def delete():
+    maybe_booking: Booking = Booking.query.get(request.args.get("id"))
+    if not maybe_booking:
+        return "Reservation not found", 404
+    else:
+        db.session.delete(maybe_booking)
+        db.session.commit()
+    return render_template("confirmation-deleted.html")
