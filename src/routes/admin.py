4b0d5b664f13c3738a76c1058a0871f0f0430db2
Felix Kressmann <felix@felixkr.de>
2021-12-12 18:43:04 +0100

    Improve Admin UI, allow confirmation and deletion of reservations

--- src/routes/admin.py
+++ src/routes/admin.py
@@ -1,12 +1,12 @@
 import datetime
 
-from flask import Blueprint, render_template, request, current_app
+from flask import Blueprint, render_template, request
+from sqlalchemy.exc import IntegrityError
 
 from extensions.db import db
 from models.Timeslot import Timeslot
 from models.Booking import Booking
 from requests.Reservation import Reservation
-from services.mail_service import send_mail
 from util import *
 
 admin_bp = Blueprint('admin', __name__)
@@ -28,8 +28,11 @@ def create():
                           vaccination=r.vaccination,
                           age=r.age,
                           ack_at=datetime.datetime.now())
-        db.session.add(booking)
-        db.session.commit()
+        try:
+            db.session.add(booking)
+            db.session.commit()
+        except IntegrityError:
+            return render_template("email-duplicated.html")
 
         return redirect(url_for('.index'))
     else:
@@ -37,6 +40,6 @@ def create():
 
 
 @admin_bp.route('/admin-print-6a3522f3-6c6e-4b0e-a044-d653177fbb91')
-def print():
+def print_bookings():
     timeslots = Timeslot.query.order_by(Timeslot.start_time).all()
     return render_template("admin-print.html", timeslots=timeslots)
